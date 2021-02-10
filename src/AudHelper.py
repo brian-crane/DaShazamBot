fc6d98191d892383fc81f2614d567427e25fe640
Brian Crane <briancrane@gmail.com>
2021-02-10 11:28:43 -0800

    Adding a Main class to test and further refactoring AudHelper

--- src/AudHelper.py
+++ src/AudHelper.py
@@ -1,19 +1,15 @@
-
-import praw
 import requests
-import Private
-
-print("I'm back baby.")
+from private import Private
 
 
-
-
-data = {
-    'api_token': Private.get_api_key(),
-    'return': 'apple_music,spotify',
-}
-files = {
-    'file': open('hello.mp3', 'rb'),
-}
-result = requests.post('https://api.audd.io/', data=data, files=files)
-print(result.text)
+# Accepts the fileName of an mp3 and sends it to AUD
+# Will return a <Success(bool), Details(String)> Response
+def post_song(mp3):
+    data = {
+        'api_token': Private.get_api_key(),
+        'return': 'apple_music,spotify',
+    }
+    files = {
+        'file': open(mp3, 'rb'),
+    }
+    return requests.post('https://api.audd.io/', data=data, files=files).text
---
+++ src/Main.py
@@ -0,0 +1,7 @@
+# This file serves as a testing ground for various features of the program, but will not be used on normal
+# circumstances of operation
+
+import AudHelper
+
+print("I'm back baby.")
+print(AudHelper.post_song("hello.mp3"))
